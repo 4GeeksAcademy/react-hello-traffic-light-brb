97f08f5789d3d9b2a18960b8a260386898928c81
Brandon-Ray Brown <77804389+BeCoded126@users.noreply.github.com>
2025-05-30 01:59:42 +0000

    project completed

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,23 +6,9 @@ import TrafficLight from "./TrafficLight";
 
 //create your first component
 const Home = () => {
-	const [ color, setColor] = useState([]);
-	const []
 
 	return (
-			<div className="container">
-			<button className= "bg-{setColor}" onClick={
-				() => setColor('success')
-				}>Light 1</button>
-
-			<button className='bg-warning' onClick={
-				() => setColor('yellow')
-				}>Light 2</button>
-
-			<button className='bg-danger' onClick={
-				() => setColor('red')
-				}>Light 3</button>
-			</div>
+			<TrafficLight/>
 
 	);
 };
--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+import redCircle from "/src/img/red-circle.png";
+import yellowCircle from "/src/img/yellow-circle.png";
+import greenCircle from "/src/img/green-circle.png";
 
 
 function TrafficLight(){
 
-return (
+        const [ yellow, setYellow] = useState();
 
-);
+        const handleClick= (light) => {
+            setYellow(light);
+        }
+        const getGlowClass = (light) => {
+                return yellow == light ? "yellow" : '';
+        }
+
+        return (
+                
+            <div className="d-inline mx-auto">
+                <button className= {`d-block ${getGlowClass("green")}`} onClick={()=> handleClick('green')}>
+                    <img className=""src={greenCircle} width="300px"/>
+                </button>
+
+                 <button className= {`d-block ${getGlowClass("yellow")}`} onClick={()=> handleClick('yellow')}>
+                    <img src={yellowCircle} width="300px"/>
+                </button>
+
+                 <button className= {`d-block ${getGlowClass("red")}`} onClick={()=> handleClick('red')}>
+                    <img src={redCircle} width="300px"/>
+                </button>
+
+            </div>
+
+        );
+
 
 }
-
-
-
-
 
 
 export default TrafficLight;
 
 
-ReactDOM.render(<TrafficLight />, document.querySelector('#app'));
+// ReactDOM.render(<TrafficLight />, document.querySelector('#app'));
